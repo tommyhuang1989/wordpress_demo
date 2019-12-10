090ace6a7c670187fca34d5626ac33ea8a11902a
tommy.huang <huangcongde1989@gmail.com>
2019-12-10 22:17:31 +0800

    add first index.php

--- wp-content/themes/tommy/index.php
+++ wp-content/themes/tommy/index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title><? bloginfo("name"); ?></title>
-        <link rel="stylesheet" href="<? bloginfo("stylesheet_url"); ?>" type="text/css"/>
+        <!-- <link rel="stylesheet" href="<? bloginfo("stylesheet_url"); ?>" type="text/css"/> -->
     </head>
     <body>
         <div>
